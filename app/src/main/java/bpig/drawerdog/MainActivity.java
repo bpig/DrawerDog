460b37ef56526ec24b28868088a3ebc912c9be6e
shuai.li <286287737@qq.com>
2015-08-30 14:58:12 +0800

    split media codes

--- app/src/main/java/bpig/drawerdog/MainActivity.java
+++ app/src/main/java/bpig/drawerdog/MainActivity.java
@@ -3,22 +3,21 @@ package bpig.drawerdog;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import static bpig.drawerdog.R.id.item_recycler;
 
 public class MainActivity extends Activity {
-
+    private MediaCapturer mediaCapturer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        getActionBar().hide();
+        mediaCapturer = new MediaCapturer(this);
         RecyclerView recyclerView = (RecyclerView) findViewById(item_recycler);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -38,33 +37,23 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-    private static final int PREVIEW_REQUEST_CODE = 1;
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings: {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(intent, PREVIEW_REQUEST_CODE);
-                }
-                return true;
-            }
+        int itemId = item.getItemId();
+        if (mediaCapturer.process(itemId)) {
+            return true;
         }
-        return super.onOptionsItemSelected(item);
+        switch (itemId) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PREVIEW_REQUEST_CODE && resultCode == RESULT_OK) {
-            Bundle extras= data.getExtras();
-            Intent intent = new Intent(this, LabelActivity.class);
-            intent.putExtras(extras);
-            startActivity(intent);
-        } else {
-            Log.w("xxxx", "error " + resultCode);
+        if (mediaCapturer.onActivityResult(requestCode, resultCode, data)) {
+            return;
         }
     }
 }
---
+++ app/src/main/java/bpig/drawerdog/MediaCapturer.java
@@ -0,0 +1,103 @@
+package bpig.drawerdog;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created: shuai.li(286287737@qq.com)
+ * Date: 2015-08-30
+ * Time: 11:43
+ */
+public class MediaCapturer {
+    private static final String TAG = "xxxx";
+    private static final int PREVIEW_REQUEST_CODE = 1;
+    private static final int SAVE_REQUEST_CODE = 2;
+
+    private Activity context;
+    public MediaCapturer(Activity context) {
+        this.context = context;
+    }
+
+    private String photoPath;
+    private File photoFile;
+    private static int photoCt = 0;
+
+    private File filename() throws IOException {
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String file = time + "_" + photoCt;
+        photoCt++;
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        Log.i(TAG, dir.toString());
+        File image = File.createTempFile(file, ".jpg", dir);
+        photoPath = "file:" + image.getAbsolutePath();
+        Log.i(TAG, photoPath);
+        return image;
+    }
+
+    public boolean process(int itemId) {
+        switch (itemId) {
+            case R.id.photo: {
+                takePhoto();
+                return true;
+            }
+            case R.id.action_settings: {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void takePhoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(context.getPackageManager()) == null) {
+            return;
+        }
+        try {
+            photoFile = filename();
+        } catch (IOException ex) {
+            Toast toast = Toast.makeText(context.getApplicationContext(), "No SD card", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        Log.w(TAG, "start photo");
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+        context.startActivityForResult(takePictureIntent, SAVE_REQUEST_CODE);
+    }
+
+    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != PREVIEW_REQUEST_CODE
+                && requestCode != SAVE_REQUEST_CODE) {
+            return false;
+        }
+        if (resultCode != Activity.RESULT_OK) {
+            Log.w(TAG, "error " + resultCode);
+            return true;
+        }
+        Log.i(TAG, "get photo " + requestCode);
+        if (requestCode == PREVIEW_REQUEST_CODE) {
+            Bundle extras = data.getExtras();
+            Intent intent = new Intent(context, LabelActivity.class);
+            intent.putExtras(extras);
+            context.startActivity(intent);
+        } else if (requestCode == SAVE_REQUEST_CODE) {
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(photoFile);
+            intent.setData(contentUri);
+            context.sendBroadcast(intent);
+//            intent = new Intent(context, LabelActivity.class);
+//            intent.putextra("uri", contentUri);
+        }
+        return true;
+    }
+}
