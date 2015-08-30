4fdc3ffc735213b3453de3f514b208d87e282092
jhumooc <jhumooc@yahoo.com>
2015-08-30 11:48:38 +0800

    basic left menu using DrawerLayout

--- app/src/main/java/bpig/drawerdog/MainActivity.java
+++ app/src/main/java/bpig/drawerdog/MainActivity.java
@@ -4,20 +4,37 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bpig.drawerdog.adapter.LeftItemAdapter;
+import bpig.drawerdog.dao.LeftMenuItem;
 
 import static bpig.drawerdog.R.id.item_recycler;
 
 public class MainActivity extends Activity {
 
+    private DrawerLayout mMainDrawerLayout;
+    private RelativeLayout mLeftMenuLayout;
+    private List<LeftMenuItem> mLeftItemList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
+        mMainDrawerLayout = (DrawerLayout)findViewById(R.id.act_main_drawerlayout);
 //        getActionBar().hide();
         RecyclerView recyclerView = (RecyclerView) findViewById(item_recycler);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -30,6 +47,8 @@ public class MainActivity extends Activity {
 
             }
         });
+
+        initLeftMenuLayout();
     }
 
     @Override
@@ -52,6 +71,9 @@ public class MainActivity extends Activity {
                 }
                 return true;
             }
+            case R.id.action_left_menu:
+                mMainDrawerLayout.openDrawer(mLeftMenuLayout);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -65,6 +87,32 @@ public class MainActivity extends Activity {
             startActivity(intent);
         } else {
             Log.w("xxxx", "error " + resultCode);
+        }
+    }
+
+    private void initLeftMenuLayout() {
+        mLeftMenuLayout = (RelativeLayout)findViewById(R.id.act_left_menu);
+        ListView leftItemListView = (ListView)findViewById(R.id.act_left_item_list);
+        mLeftItemList = new ArrayList<LeftMenuItem>();
+        mLeftItemList.add(new LeftMenuItem(android.R.drawable.ic_menu_gallery, "首页"));
+        mLeftItemList.add(new LeftMenuItem(android.R.drawable.star_big_off, "我的药箱"));
+        mLeftItemList.add(new LeftMenuItem(android.R.drawable.ic_menu_manage, "设置"));
+        mLeftItemList.add(new LeftMenuItem(android.R.drawable.ic_menu_close_clear_cancel, "退出登录"));
+
+        LeftItemAdapter adapter = new LeftItemAdapter(this, mLeftItemList);
+        leftItemListView.setAdapter(adapter);
+    }
+
+    private class LeftDrawerItemClick implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            switch (position) {
+                case 0:
+                    mMainDrawerLayout.closeDrawer(mLeftMenuLayout);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ app/src/main/java/bpig/drawerdog/adapter/LeftItemAdapter.java
@@ -0,0 +1,67 @@
+package bpig.drawerdog.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import bpig.drawerdog.R;
+import bpig.drawerdog.dao.LeftMenuItem;
+
+public class LeftItemAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<LeftMenuItem> mItemList;
+
+    public LeftItemAdapter(Context context, List<LeftMenuItem> itemList) {
+        super();
+        this.mContext = context;
+        this.mItemList = itemList;
+    }
+
+    @Override
+    public int getCount() {
+        if (this.mItemList != null) {
+            return mItemList.size();
+        }
+        return 0;
+    }
+    @Override
+    public Object getItem(int position) {
+        if (mItemList != null) {
+            return mItemList.get(position);
+        }
+        return null;
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup paret) {
+        ItemViewHold hold;
+        if (convertView == null) {
+            hold = new ItemViewHold();
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_left_menu, null);
+            convertView.setTag(hold);
+        } else {
+            hold = (ItemViewHold)convertView.getTag();
+        }
+        hold.mImageView = (ImageView)convertView.findViewById(R.id.left_item_icon);
+        hold.mTextView = (TextView)convertView.findViewById(R.id.left_item_name);
+
+        hold.mImageView.setImageResource(mItemList.get(position).getItemImage());
+        hold.mTextView.setText(mItemList.get(position).getItemName());
+
+        return convertView;
+    }
+
+    private static class ItemViewHold {
+        public ImageView mImageView;
+        public TextView mTextView;
+    }
+}
